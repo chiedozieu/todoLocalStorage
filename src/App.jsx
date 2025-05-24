f235ae279bcabbf4cc35f0e3a6cc493383be4aba
chiedozieu <chiedozieugwor@gmail.com>
2025-05-24 15:24:40 -0400

    create function addTodo, updateTodo, deleteTodo and toggleComplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
+  const [todos, setTodos ] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev ])
+  }
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((t) => t.id === todo.id ? todo : t))
+  }
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((t) => t.id !== id))
+  }
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((t) => t.id === id ? {...t, completed: !t.completed} : t))
+  }
   
 
   return (
-    <>
+    <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
       <h1 className='text-red-800'>Hello</h1>
-    </>
+    </TodoProvider>
   )
 }
 
