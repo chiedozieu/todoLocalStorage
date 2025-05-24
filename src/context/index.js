02f870f0c76d2c435ef830e974223c611d9507f7
chiedozieu <chiedozieugwor@gmail.com>
2025-05-24 14:34:20 -0400

    create vite@latest, install tailwindcss, do basic setup

---
+++ src/context/index.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+
+export const todoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            title: 'Todo 1',
+            description: 'Description 1',
+            completed: false
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(todoContext)
+}
+
+export const TodoProvider = todoContext.Provider
